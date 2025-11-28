8004ec42a4eabeb7f1075a305ff44adc6d09e82f
Lindor <radebelindo14@gmail.com>
2025-11-28 21:11:00 +0200

    hmmm

---
+++ navbar.jsx
@@ -0,0 +1,10 @@
+import { userRef, userState } from "react";
+import ".NavigationBar.css";
+
+const items = [
+    {
+        name: "About", items: ["Following" ...],
+    },]
+
+
+    // <!--STill need to get used to this type of stuff-->
